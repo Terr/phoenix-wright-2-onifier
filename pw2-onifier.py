8b56cbddc921a92b2fe9f2b691a2807104058731
Terr <git@terr.nl>
2024-04-30 22:28:51 +0200

    Remap instrument wave ID to their position in PW2's WAVE_AGB_BGM
    Also copy some waves from PW1 that sound slightly different than the
    ones present in PW2's WAVE_AGB_BGM.

--- pw2-onifier.py
+++ pw2-onifier.py
@@ -7,10 +7,11 @@ import sys
 from ndspy import findInNamedList, indexInNamedList, setInNamedList
 from ndspy.rom import NintendoDSRom
 from ndspy.soundArchive import SDAT
+from ndspy.soundBank import SingleNoteInstrument, RangeInstrument
 
 
 # The key in this dictionary is the filename the track has in Phoenix Wright 1,
-# the value is the track to be replaced in Phoenix Wright 2
+# the value is the track to be replaced in Phoenix Wright 2.
 #
 # In the comment is roughly the name of the track in Phoenix Wright 1.
 MUSIC_MAPPING = {
@@ -23,6 +24,35 @@ MUSIC_MAPPING = {
     "BGM002": "BGM074",  # Pursuit
     "BGM003": "BGM075",  # Pursuit variation
     "BGM016": "BGM080",  # Save prompt
+}
+
+# Some of the wave samples in PW2's WAVE_AGB_BGM sound slightly different (less
+# muffled) than PW1's, so copy those original waves to PW2 to keep the PW1 music
+# sounding as it should.
+WAVES_TO_COPY = [24, 25, 26, 28, 17, 16]
+
+# Some wave sounds that are used by PW1's music have gotten a different position
+# in PW2's WAVE_AGB_BGM.
+#
+# Most of the waves have just shifted by one position.
+#
+# The key in the dictionary is the wave ID in PW1, the value the ID in PW2.
+WAVE_ID_MAPPING = {
+    14: 15,
+    15: 16,
+    16: 56,
+    17: 55,
+    18: 19,
+    19: 20,
+    20: 21,
+    21: 22,
+    22: 23,
+    23: 24,
+    24: 51,
+    25: 52,
+    26: 53,
+    28: 54,
+    33: 34,
 }
 
 
@@ -55,14 +85,13 @@ def onify(pw1_rom_path: str, pw2_rom_path: str, output_path: str):
 def copy_sound_files(mapping: dict, source_sdat: SDAT, destination_sdat: SDAT):
     """Copies the sound files from the source SDAT to the destination SDAT."""
 
-    # Make a complete copy of WAVE_AGB_BGM, used by some PW1 songs, but don't
-    # overwrite PW2's version. Instead, just point the PW1 song banks to use
-    # PW1's WAVE_AGB_BGM.
-    pw1_agb_bgm = findInNamedList(source_sdat.waveArchives, "WAVE_AGB_BGM")
-    destination_sdat.waveArchives.append(("WAVE_AGB_BGM_PW1", pw1_agb_bgm))
-    pw1_agb_bgm_index = indexInNamedList(
-        destination_sdat.waveArchives, "WAVE_AGB_BGM_PW1"
-    )
+    # Copy some instrument wave from PW1 to PW2 that sound different from the waves present in PW2
+    pw1_wave_agb_bgm = findInNamedList(source_sdat.waveArchives, "WAVE_AGB_BGM")
+    pw2_wave_agb_bgm = findInNamedList(destination_sdat.waveArchives, "WAVE_AGB_BGM")
+
+    for wave_id in WAVES_TO_COPY:
+        # Because PW2 has 51 waves (0-indexed, so 0-50), the first wave that's copied will become wave 51
+        pw2_wave_agb_bgm.waves.append(pw1_wave_agb_bgm.waves[wave_id])
 
     for from_name, to_name in mapping.items():
         wave = findInNamedList(source_sdat.waveArchives, f"WAVE_{from_name}")
@@ -73,9 +102,16 @@ def copy_sound_files(mapping: dict, source_sdat: SDAT, destination_sdat: SDAT):
         bank.waveArchiveIDs = findInNamedList(
             destination_sdat.banks, f"BANK_{to_name}"
         ).waveArchiveIDs
-        # If the bank uses WAVE_AGB_BGM, point it to PW1's version
-        if bank.waveArchiveIDs[0] == 0:
-            bank.waveArchiveIDs[0] = pw1_agb_bgm_index
+        # Do a mapping of the wave IDs to the position in PW2's WAVE_AGB_BGM
+        for instrument in bank.instruments:
+            if isinstance(instrument, SingleNoteInstrument):
+                if pw2_wave_id := WAVE_ID_MAPPING.get(instrument.noteDefinition.waveID):
+                    instrument.noteDefinition.waveID = pw2_wave_id
+            elif isinstance(instrument, RangeInstrument):
+                for note_definition in instrument.noteDefinitions:
+                    if pw2_wave_id := WAVE_ID_MAPPING.get(note_definition.waveID):
+                        note_definition.waveID = pw2_wave_id
+
         setInNamedList(destination_sdat.banks, f"BANK_{to_name}", bank)
 
         seq = findInNamedList(source_sdat.sequences, from_name)
